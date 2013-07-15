ddb40762062374fe1c7133415d46e3f5bb421768
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2013-07-14 21:39:19 -0400

    Added FormUnapplier helper for case class extraction in forms.
    This now works:
    
      case class User(name: String, password: String, age: Int)
      object FormUser extends FormUnapplier(User.apply _, User.unapply _)
    
      def fieldChanged(newValue: String) = {
        Run("console.log('Saw value ' + newValue + ' on the server.')")
      }
    
      val nameField =
        field[String](".name") ? notEmpty -> on("change", fieldChanged _)
      val passwordField =
        field[String]("type=password") ? notEmpty -> on("change",
      fieldChanged _)
      val age =
        field[Int](".age") ? inRange(0, 130)
    
      val userForm =
        form withField nameField withField passwordField withField age
      formalize().
          onSuccess {
            case FormUser(person) =>
              // do stuff with person
          } onFailure {
            case Full(name) :: _ :: _ :: HNil =>
              println("At least the name was present!")
            case _ =>
              println("Can't get anything right...")
          }
    
      "form" #> userForm.binder()

---
+++ src/main/scala/com/withoutincident/formality/FormUnapplier.scala
@@ -0,0 +1,22 @@
+package com.withoutincident
+package formality
+
+import shapeless._
+
+class FormUnapplier[
+  ApplyType, UnapplyParameterType,
+  CaseClassType <: Product,
+  ParameterHList <: HList
+](
+  apply: ApplyType,
+  unapply: UnapplyParameterType => Option[CaseClassType]
+)(
+  implicit fnHlister: FnHListerAux[ApplyType, ParameterHList => UnapplyParameterType],
+           hlister: HListerAux[CaseClassType, ParameterHList]
+) {
+  val iso = Iso.hlist(apply, unapply)
+
+  def unapply(hlist: ParameterHList): Option[UnapplyParameterType] = {
+    Some(iso.from(hlist))
+  }
+}
