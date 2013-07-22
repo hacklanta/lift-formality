13ef2bc1c4c45d5db3b1002b0eb5c9fc0b102bf6
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2013-07-21 21:14:49 -0400

    Rename S context for specs, add SessionContext base trait.
    The SessionContext exposes the sessions for tests to use if necessary.

--- src/test/scala/com/withoutincident/formality/FieldSpec.scala
+++ src/test/scala/com/withoutincident/formality/FieldSpec.scala
@@ -12,9 +12,11 @@ import net.liftweb.util._
 
 import Formality._
 
-object withSContext extends Around {
+trait SessionContext {
+  val session = new LiftSession("", StringHelpers.randomString(20), Empty)
+}
+trait SContext extends Around with SessionContext {
   def around[T <% Result](t: =>T) = {
-    val session = new LiftSession("", StringHelpers.randomString(20), Empty)
     S.initIfUninitted(session) {
       AsResult(t)  // execute t inside a http session
     }
@@ -24,8 +26,8 @@ object withSContext extends Around {
 class FieldSpec extends Specification {
   val templateElement = <div class="boomdayada boomdayadan" data-test-attribute="bam">Here's a test!</div>
 
-  "Formality when creating simple fields with no initial value" should {
-    "only bind the name attribute" in withSContext {
+  "Simple fields with no initial value" should {
+    "only bind the name attribute" in new SContext {
       val formField = field[String](".boomdayada")
 
       val resultingMarkup = <test-parent>{formField.binder(templateElement)}</test-parent>
@@ -40,8 +42,8 @@ class FieldSpec extends Specification {
     }
   }
   
-  "Formality when creating simple fields with an initial value" should {
-    "only bind the name and value attributes" in withSContext {
+  "Simple fields with an initial value" should {
+    "only bind the name and value attributes" in new SContext {
       val formField = field[String](".boomdayada", "Dat value")
 
       val resultingMarkup = <test-parent>{formField.binder(templateElement)}</test-parent>
@@ -56,8 +58,8 @@ class FieldSpec extends Specification {
     }
   }
 
-  "Formality when creating regular file upload fields" should {
-    "only bind the name and type attributes" in withSContext {
+  "Regular file upload fields" should {
+    "only bind the name and type attributes" in new SContext {
       val formField = fileUploadField(".boomdayada")
 
       val resultingMarkup = <test-parent>{formField.binder(templateElement)}</test-parent>
@@ -72,8 +74,8 @@ class FieldSpec extends Specification {
     }
   }
 
-  "Formality when creating typed file upload fields" should {
-    "only bind the name and type attributes" in withSContext {
+  "Typed file upload fields" should {
+    "only bind the name and type attributes" in new SContext {
       implicit def fileToObject(fph: FileParamHolder) = Full("boom")
 
       val formField = typedFileUploadField[String](".boomdayada")
