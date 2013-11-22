e630ebfbb8cd703773b9414733dc726330f78ccd
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2013-11-21 23:11:32 -0500

    Do an initial bind of the signup form in the simple chat example.

---
+++ examples/simple-chat/src/main/scala/code/model/User.scala
@@ -0,0 +1,12 @@
+package code
+package model
+
+case class User(email: String, password: String)
+
+object User {
+  @volatile var users = List[User]()
+
+  def create(user: User) = {
+    users ::= user
+  }
+}
---
+++ examples/simple-chat/src/main/scala/code/snippet/Signup.scala
@@ -0,0 +1,28 @@
+package code
+package snippet
+
+import net.liftweb.http.SHtml
+import net.liftweb.util.Helpers._
+
+import com.hacklanta.formality.Formality
+  import Formality._
+
+import model.User
+
+class Signup {
+  def form = {
+    val registrationForm =
+      Formality.form withField
+        field[String]("#email") withField
+        field[String]("#password") formalize() onSuccess {
+          case email :+: password :+: HNil =>
+            User.create(User(email, password))
+
+            net.liftweb.http.js.JsCmds.Alert("Bam!")
+      }
+
+    SHtml.makeFormsAjax andThen
+    "form" #> registrationForm.binder()
+  }
+}
+
