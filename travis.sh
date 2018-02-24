878f449dc62492309b22f67ce6303bf403c7ed6f
Antonio Salazar Cardozo <antonio@keep.network>
2018-02-24 12:38:35 -0500

    Only publish snapshots for master branch

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -ev
 
 sbt ++$TRAVIS_SCALA_VERSION "set liftVersion := \"$LIFT_VERSION\"" test
 
-if grep -q -e "version :=.*SNAPSHOT" build.sbt && [ "${TRAVIS_PULL_REQUEST}" == "false" ] ; then
+if grep -q -e "version :=.*SNAPSHOT" build.sbt && [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ] ; then
   echo "Publishing snapshot to sonatype..."
 
   openssl aes-256-cbc -K $encrypted_ddfe6500c86a_key -iv $encrypted_ddfe6500c86a_iv -in .sonatype.enc -out ~/.sonatype -d
