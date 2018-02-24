ab663863a7276050054f94cef06a02843ae38d6d
Antonio Salazar Cardozo <antonio@keep.network>
2018-02-24 12:14:14 -0500

    Cross-build Lift versions
    We build Lift 3.0, 3.1, 3.2 against Scala 2.11 and Scala 2.12, and we build
    Lift 2.6 against Scala 2.11.

--- travis.sh
+++ travis.sh
@@ -2,14 +2,14 @@
 
 set -ev
 
-sbt ++$TRAVIS_SCALA_VERSION test
+sbt ++$TRAVIS_SCALA_VERSION "set liftVersion := \"$LIFT_VERSION\"" test
 
 if grep -q -e "version :=.*SNAPSHOT" build.sbt && [ "${TRAVIS_PULL_REQUEST}" == "false" ] ; then
   echo "Publishing snapshot to sonatype..."
 
   openssl aes-256-cbc -K $encrypted_ddfe6500c86a_key -iv $encrypted_ddfe6500c86a_iv -in .sonatype.enc -out ~/.sonatype -d
 
-  sbt ++$TRAVIS_SCALA_VERSION publish
+  sbt ++$TRAVIS_SCALA_VERSION "set liftVersion := \"$LIFT_VERSION\"" publish
 
   rm ~/.sonatype
 fi
